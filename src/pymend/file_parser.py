72fa7a23ec3f5890674e9887552816f78bf00582
Jan-Eric Nitschke <47750513+JanEricNitschke@users.noreply.github.com>
2026-03-08 11:48:24 +0100

    Update version

--- src/pymend/file_parser.py
+++ src/pymend/file_parser.py
@@ -25,7 +25,7 @@ from .docstring_info import (
 __author__ = "J-E. Nitschke"
 __copyright__ = "Copyright 2023-2024"
 __licence__ = "MIT"
-__version__ = "2.0.1"
+__version__ = "2.1.0"
 __maintainer__ = "J-E. Nitschke"
 
 
